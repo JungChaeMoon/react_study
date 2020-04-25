9897842062ce26ed980c028a25266dcfddc0f06f
JungChaeMoon <jungchaemoon@gmail.com>
2020-04-25 20:12:32 +0900

    Hook useReducer

--- src/App.js
+++ src/App.js
@@ -13,14 +13,8 @@ import Info from "./Info";
 
 
 const App = () => {
-    const [visible, setVisible] = useState(false);
     return (
-        <div>
-            <button onClick={() => {
-                setVisible(!visible)
-            }}>{visible ? '숨기기' : '보이기'}</button><hr />
-            {visible && <Info />}
-        </div>
+        <Info />
     );
 };
 
